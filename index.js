0db48e169fb7738015b8341497e9659c23ba3758
jspl151 <23327100-jspl151@users.noreply.replit.com>
2023-05-26 14:15:02 +0000

    Improved.

--- index.js
+++ index.js
@@ -29,39 +29,32 @@ var purchases = [
     units: 1,
   }
 ];
+
 const getDiscountPercent = (productName) => discounts[productName] / 100 || 0;
-//const getDiscountPercent = (productName)=> discounts[productName] ? discounts[productName]/100 : 0 ;
 
 const getTaxPercent = (productName) => taxes[productName] / 100 || 0;
 
 const getUnitPrice = (itemName) => {
-  let discounts = rates[itemName] * getDiscountPercent(itemName);
-  let tax = rates[itemName] * getTaxPercent(itemName);
-  let unitPrice = rates[itemName] - discounts + tax;
-  return unitPrice;
+  const discount = rates[itemName] * getDiscountPercent(itemName);
+  const tax = rates[itemName] * getTaxPercent(itemName);
+  const unitPrice = rates[itemName] - discount + tax;
+  
+  return {discount,tax,unitPrice};
 };
 
-const getLineItemPrice = (lineItem) => lineItem.units * getUnitPrice(lineItem.item)
+const getLineItemPrice = (lineItem) => {
+  const { discount , tax , unitPrice}=getUnitPrice(lineItem.item);
+  const subTotal = lineItem.units * unitPrice;
+
+  return {...lineItem ,discount , tax , subTotal};
+};
 
 const getSum = () => {
-let total = purchases.reduce((acc, currentvalue) => acc + 
-  getLineItemPrice(currentvalue), 0);
-  
-  /* let lineItemPriceList=purchases.map((item,i)=>
-     getLineItemPrice(purchases[i]));
-   let total=lineItemPriceList.reduce((acc,currentvalue)=> acc+currentvalue,0);
- return total;
-  const a= purchases.map((item,i)=>
-     {
-       total+=getLineItemPrice(purchases[i])
-     });*/
- /* let total=0;
-   for(let i=0;i<purchases.length;i++)
-   {
-     total+=getLineItemPrice(purchases[i]);
-   }*/
- 
-   return total
+  const lineItemPriceList=purchases.map((purchase)=> getLineItemPrice(purchase));
+  console.log(lineItemPriceList);
+  const total=lineItemPriceList.reduce((acc,currentvalue)=> acc + currentvalue.subTotal,0);
+
+  return total;
 };
 
 const main = () => console.log(getSum());
