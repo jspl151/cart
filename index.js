b4c3f6f00387037de1c00a9c1060ae02af33e3a9
jspl151 <133941938+jspl151@users.noreply.github.com>
2023-05-27 05:11:23 +0000

    console.table

--- index.js
+++ index.js
@@ -30,32 +30,38 @@ var purchases = [
   }
 ];
 
-const getDiscountPercent = (productName) => discounts[productName] / 100 || 0;
+const getDiscountPercent = (productName) =>
+  discounts[productName] / 100 || 0;
 
-const getTaxPercent = (productName) => taxes[productName] / 100 || 0;
+const getTaxPercent = (productName) =>
+  taxes[productName] / 100 || 0;
 
 const getUnitPrice = (itemName) => {
   const discount = rates[itemName] * getDiscountPercent(itemName);
   const tax = rates[itemName] * getTaxPercent(itemName);
   const unitPrice = rates[itemName] - discount + tax;
-  
-  return {discount,tax,unitPrice};
+
+  return { discount, tax, unitPrice };
 };
 
 const getLineItemPrice = (lineItem) => {
-  const { discount , tax , unitPrice}=getUnitPrice(lineItem.item);
+  const { discount, tax, unitPrice } = getUnitPrice(lineItem.item);
   const subTotal = lineItem.units * unitPrice;
 
-  return {...lineItem ,discount , tax , subTotal};
+  return { ...lineItem, discount, tax, subTotal };
 };
 
 const getSum = () => {
-  const lineItemPriceList=purchases.map((purchase)=> getLineItemPrice(purchase));
-  console.log(lineItemPriceList);
-  const total=lineItemPriceList.reduce((acc,currentvalue)=> acc + currentvalue.subTotal,0);
-
-  return total;
+  const lineItemPriceList = purchases.map((purchase) =>
+    getLineItemPrice(purchase));
+  const total = lineItemPriceList.reduce((acc, currentvalue) =>
+    acc + currentvalue.subTotal, 0);
+  return { total, lineItemPriceList };
 };
 
-const main = () => console.log(getSum());
+const main = () => {
+  const { lineItemPriceList, total } = getSum();
+  console.table(lineItemPriceList);
+  console.log(total);
+}
 main();
